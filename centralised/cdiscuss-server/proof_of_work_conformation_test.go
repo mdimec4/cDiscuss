1c30a4a95f8491b7175399cd8ebf6861acacb9e2
Miha Dimec <miha.dimec@visionect.com>
2024-06-09 13:45:48 +0200

    POW cont hash leading zero bits

--- centralised/cdiscuss-server/proof_of_work_conformation_test.go
+++ centralised/cdiscuss-server/proof_of_work_conformation_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"crypto/sha256"
 	"testing"
 	"time"
 )
@@ -26,6 +27,16 @@ func TestParsePowToken(t *testing.T) {
 		t.Errorf("Wrong timestamp: %v", parsedPowToken.dtCreatedReported)
 	}
 	if parsedPowToken.randNum != 3764117886647529 {
-		t.Errorf("Wrong randNum: %v", parsedPowToken.randNum)
+		t.Errorf("Wrong randNum: %d", parsedPowToken.randNum)
+	}
+}
+
+func TestCountSha256LeadingZeroBits(t *testing.T) {
+	const token = "19:adam:1717855224906:3764117886647529"
+
+	var sumSha256 [sha256.Size]byte = sha256.Sum256([]byte(token))
+	hashLeadingZeroBitCount := countSha256LeadingZeroBits(sumSha256)
+	if hashLeadingZeroBitCount != 19 {
+		t.Errorf("Wrong computed leading zero bits count: %d", hashLeadingZeroBitCount)
 	}
 }
