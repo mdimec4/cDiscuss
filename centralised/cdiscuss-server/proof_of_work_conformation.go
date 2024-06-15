b85fd6e3c688fa16ce7737e00adb55cd7c084e5c
Miha Dimec <miha.dimec@visionect.com>
2024-06-15 06:12:09 +0200

    pow itf

--- centralised/cdiscuss-server/proof_of_work_conformation.go
+++ centralised/cdiscuss-server/proof_of_work_conformation.go
@@ -5,8 +5,6 @@ import (
 	"errors"
 	"fmt"
 	"log/slog"
-	"strconv"
-	"strings"
 	"sync"
 	"time"
 )
@@ -75,10 +73,6 @@ func (powConform *proofOfWorkConformation) deleteOutdatedTokens() {
 		}
 		return true
 	})
-}
-
-func isExpired(now time.Time, expiresTime time.Time) bool {
-	return !now.Before(expiresTime)
 }
 
 func (powConform *proofOfWorkConformation) stop() {
@@ -161,51 +155,6 @@ func (powConform *proofOfWorkConformation) isTokenUsedAndForgetIfExpired(token s
 	return tokenFound, nil
 }
 
-type parsedPowToken struct {
-	hardnes           uint
-	username          string
-	dtCreatedReported time.Time
-	randNum           int64
-}
-
-// POW tokens look like 19:adam:1717855224906:3764117886647529
-func parsePowToken(token string) (*parsedPowToken, error) {
-	const tokenPartCount int = 4
-
-	var tokenParts []string = strings.SplitN(token, ":", tokenPartCount)
-	if len(tokenParts) != tokenPartCount {
-		slog.Error("POW token wrong parts count")
-		return nil, errInvalidPowToken
-	}
-
-	hardnesStr := tokenParts[0]
-	username := tokenParts[1]
-	timestampStr := tokenParts[2]
-	randNumStr := tokenParts[3]
-
-	hardnes, err := strconv.ParseUint(hardnesStr, 10, 32)
-	if err != nil {
-		slog.Error("parse POW hardnes", slog.Any("error", err))
-		return nil, errInvalidPowToken
-	}
-	timestamp, err := strconv.ParseInt(timestampStr, 10, 64)
-	if err != nil {
-		slog.Error("parse POW timestamp", slog.Any("error", err))
-		return nil, errInvalidPowToken
-	}
-	dtCreatedReported := time.UnixMilli(timestamp)
-
-	randNum, err := strconv.ParseInt(randNumStr, 10, 64)
-	if err != nil {
-		slog.Error("parse POW randnum", slog.Any("error", err))
-		return nil, errInvalidPowToken
-	}
-
-	parsedToken := &parsedPowToken{hardnes: uint(hardnes), username: username,
-		dtCreatedReported: dtCreatedReported, randNum: randNum}
-	return parsedToken, nil
-}
-
 func (powConform *proofOfWorkConformation) isTokenAceptableStore(token string, requiredHardnes uint, username string) (bool, error) {
 	isTokenUsed, err := powConform.isTokenUsedAndForgetIfExpired(token)
 	if err != nil {
@@ -242,19 +191,4 @@ func (powConform *proofOfWorkConformation) isTokenAceptableStore(token string, r
 
 	powConform.storeToken(token, now)
 	return true, nil
-}
-
-func countSha256LeadingZeroBits(sumSha256 [sha256.Size]byte) uint {
-	var zeroBitCount uint = 0
-
-	for i := 0; i < sha256.Size; i++ {
-		for j := 0; j < 8; j++ {
-			if ((sumSha256[i] >> (7 - j)) & 0x01) == 0 {
-				zeroBitCount++
-			} else {
-				return zeroBitCount
-			}
-		}
-	}
-	return zeroBitCount
 }
---
+++ centralised/cdiscuss-server/proof_of_work_conformation_itf.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"crypto/sha256"
+	"log/slog"
+	"strconv"
+	"strings"
+	"time"
+)
+
+type proofOfWorkConformationItf interface {
+	isTokenAceptableStore(token string, requiredHardnes uint, username string) (bool, error)
+}
+
+func isExpired(now time.Time, expiresTime time.Time) bool {
+	return !now.Before(expiresTime)
+}
+
+type parsedPowToken struct {
+	hardnes           uint
+	username          string
+	dtCreatedReported time.Time
+	randNum           int64
+}
+
+// POW tokens look like 19:adam:1717855224906:3764117886647529
+func parsePowToken(token string) (*parsedPowToken, error) {
+	const tokenPartCount int = 4
+
+	var tokenParts []string = strings.SplitN(token, ":", tokenPartCount)
+	if len(tokenParts) != tokenPartCount {
+		slog.Error("POW token wrong parts count")
+		return nil, errInvalidPowToken
+	}
+
+	hardnesStr := tokenParts[0]
+	username := tokenParts[1]
+	timestampStr := tokenParts[2]
+	randNumStr := tokenParts[3]
+
+	hardnes, err := strconv.ParseUint(hardnesStr, 10, 32)
+	if err != nil {
+		slog.Error("parse POW hardnes", slog.Any("error", err))
+		return nil, errInvalidPowToken
+	}
+	timestamp, err := strconv.ParseInt(timestampStr, 10, 64)
+	if err != nil {
+		slog.Error("parse POW timestamp", slog.Any("error", err))
+		return nil, errInvalidPowToken
+	}
+	dtCreatedReported := time.UnixMilli(timestamp)
+
+	randNum, err := strconv.ParseInt(randNumStr, 10, 64)
+	if err != nil {
+		slog.Error("parse POW randnum", slog.Any("error", err))
+		return nil, errInvalidPowToken
+	}
+
+	parsedToken := &parsedPowToken{hardnes: uint(hardnes), username: username,
+		dtCreatedReported: dtCreatedReported, randNum: randNum}
+	return parsedToken, nil
+}
+
+func countSha256LeadingZeroBits(sumSha256 [sha256.Size]byte) uint {
+	var zeroBitCount uint = 0
+
+	for i := 0; i < sha256.Size; i++ {
+		for j := 0; j < 8; j++ {
+			if ((sumSha256[i] >> (7 - j)) & 0x01) == 0 {
+				zeroBitCount++
+			} else {
+				return zeroBitCount
+			}
+		}
+	}
+	return zeroBitCount
+}
